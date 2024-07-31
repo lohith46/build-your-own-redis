aa468cc8581dc55df6f9ab6afac47a003b111e27
lohith46 <lohithkm46@gmail.com>
2024-07-31 11:43:05 +0530

    added tests for String formatter

--- src/main/java/formatters/StringFormatter.java
+++ src/main/java/formatters/StringFormatter.java
@@ -7,7 +7,7 @@ public class StringFormatter implements ValueFormatter {
   @Override
   public String format(Object value) {
     String stringValue = (String) value;
-    if (stringValue.isEmpty()) {
+    if (stringValue.isBlank()) {
       return BULK_STRINGS + -1 + CRLF;
     }
     return SIMPLE_STRING + stringValue + CRLF;
--- src/test/java/formatters/StringFormatterTest.java
+++ src/test/java/formatters/StringFormatterTest.java
@@ -1,4 +1,36 @@
+package formatters;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static utils.Constants.*;
+
 class StringFormatterTest {
-  
+
+  StringFormatter formatter = new StringFormatter();
+
+  public static Stream<Arguments> testData() {
+    return Stream.of(
+      Arguments.of("a"),
+      Arguments.of("Hello World!"),
+      Arguments.of("How are you doing??")
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource(value = "testData")
+  void shouldReturnDefaultStringIrrespectiveOfValuePassed(String value) {
+    String formattedString = formatter.format(value);
+    assertEquals( SIMPLE_STRING + value + CRLF, formattedString);
+  }
+
+  @Test
+  void shouldReturnNullStringIfGivenStringIsNullOrEmpty() {
+    String formattedString = formatter.format("");
+    assertEquals( BULK_STRINGS + -1 + CRLF, formattedString);
+  }
 }
